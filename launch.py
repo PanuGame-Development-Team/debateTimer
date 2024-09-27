ad8802b0c898981a02533136a346cc0ce391e3c3
BillsgWu <BillsgWu@outlook.com>
2024-09-27 15:03:22 +0800

    1.0-240947-R1-final

--- launch.py
+++ launch.py
@@ -22,9 +22,9 @@ class Window(QWidget):
         if self.status == 0:
             dtime = eval(self.ui.time1.text())
             self.status = 1
-        elif self.status == 1:
+        elif int(self.status) == 1:
             dtime = eval(self.ui.time2.text())
-            self.status = 2
+            self.status += 0.25
         elif self.status == 2:
             dtime = eval(self.ui.time3.text())
             self.status = 3
@@ -34,36 +34,42 @@ class Window(QWidget):
             dtime = eval(self.ui.time4.text())
             self.status = -1
         # PlayerWidget(dtime=dtime).exec()
-        popen("./player dtime=%d"%dtime).read()
+        popen("player.exe dtime=%d"%dtime).read()
         if self.status == -1:
             self.close()
         self.repaint()
     @pyqtSlot()
     def on_passstep_clicked(self):
-        self.status += 1
+        if self.status >= 1 and self.status < 2:
+            self.status += 0.25
+        else:
+            self.status += 1
         if self.status > 4:
             self.close()
         self.repaint()
     def paintEvent(self,event):
-        if self.status > 3:
+        if self.status >= 4:
             self.ui.label3a.setDisabled(True)
             self.ui.second3a.setDisabled(True)
             self.ui.time3a.setDisabled(True)
             y = 190
-        elif self.status > 2:
+        elif self.status >= 3:
             self.ui.label3.setDisabled(True)
             self.ui.second3.setDisabled(True)
             self.ui.time3.setDisabled(True)
             y = 150
-        elif self.status > 1:
+        elif self.status >= 2:
             self.ui.label2.setDisabled(True)
             self.ui.second2.setDisabled(True)
             self.ui.time2.setDisabled(True)
+            self.ui.round.setDisabled(True)
+            self.ui.round_text.setDisabled(True)
             y = 110
-        elif self.status > 0:
+        elif self.status >= 1:
             self.ui.label1.setDisabled(True)
             self.ui.second1.setDisabled(True)
             self.ui.time1.setDisabled(True)
+            self.ui.round.setText(str(int(self.status*4-3)))
             y = 70
         else:
             y = 30
